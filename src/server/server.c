ba48bb844d7e408cd8e118020f4c626adaa42df6
anshonweb <ansh.a.3112@gmail.com>
2025-05-18 23:26:03 +0530

    got the server working

--- src/server/server.c
+++ src/server/server.c
@@ -1,58 +1,40 @@
 #include "server.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
+#include <arpa/inet.h>  // For htons, htonl
 
 struct Server server_constructor(int domain, int service, int protocol,
-                                 int port, int backlog, u_long interface,
-                                void (*start)(struct Server *server)){
+                                 int port, int backlog, uint32_t interface,
+                                 void (*start)(struct Server *server)) {
+    struct Server server;
 
+    server.domain = domain;
+    server.service = service;
+    server.protocol = protocol;
+    server.port = port;
+    server.backlog = backlog;
+    server.interface = interface;
+    server.start = start;
 
-struct Server server;
-server.domain = domain;
-server.service =  service;
-server.protocol = protocol;
-server.port = port;
-server.backlog = backlog;
-server.interface = interface;
-server.start = start;
+    server.address.sin_family = domain;
+    server.address.sin_port = htons(port);
+    server.address.sin_addr.s_addr = htonl(interface);
 
+    server.sock = socket(domain, service, protocol);
+    if (server.sock == 0) {
+        perror("Failed to create socket");
+        exit(1);
+    }
 
+    if (bind(server.sock, (struct sockaddr*)&server.address, sizeof(server.address)) < 0) {
+        perror("Failed to bind socket");
+        exit(1);
+    }
 
-server.address.sin_family = domain;
-server.address.sin_port = htons(port); //converts int to network byte type htons
-server.address.sin_addr.s_addr = htonl(interface);
+    if (listen(server.sock, server.backlog) < 0) {
+        perror("Failed to start listening");
+        exit(1);
+    }
 
-
-server.socket = socket(domain , service, protocol);
-
-if (server.socket == 0){
-
-    perror("Failed to connect to socket..\n");
-    exit(1);
+    return server;
 }
-
-
-if ((bind(server.socket, (struct sockaddr*)&server.address, sizeof(server.address))) < 0){
-
-    perror("Failed to bind socket... \n");
-    exit(1);
-
-}
-
-if((listen(server.socket, server.backlog)) <0){
-
-    perror("Failed to start listening...\n");
-    exit(1);
-}
-
-server.launch = launch;
-
-return server;
-
-
-
-
-
-                                    
-                                }
--- src/server/server.h
+++ src/server/server.h
@@ -4,12 +4,12 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <sys/types.h>
+#include <stdint.h>        // for uint32_t
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
-struct Server{
-
+struct Server {
     int domain;
     int type;
     int protocol;
@@ -17,17 +17,14 @@ struct Server{
     int service; 
     int port;
     int backlog;
-    u_long interface;
+    uint32_t interface;
     struct sockaddr_in address;
     int sock;
-    void(*start)(struct Server *server);
-
+    void (*start)(struct Server *server);
 };
 
-
 struct Server server_constructor(int domain, int service, int protocol,
-                                 int port, int backlog, u_long interface,
+                                 int port, int backlog, uint32_t interface,
                                  void (*start)(struct Server *server));
 
-
-#endif 
+#endif
